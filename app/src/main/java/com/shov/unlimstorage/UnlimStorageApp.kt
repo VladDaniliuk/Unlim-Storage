d4a388c758b31958fbb9d52ceddcdcd9956a8822
Vladislav Daniliuk <48360685+VladDaniliuk@users.noreply.github.com>
2021-07-22 15:57:31 +0300

    Add Timber for testing

--- app/src/main/java/com/shov/unlimstorage/UnlimStorageApp.kt
+++ app/src/main/java/com/shov/unlimstorage/UnlimStorageApp.kt
@@ -1,7 +1,17 @@
 package com.shov.unlimstorage
 
 import android.app.Application
+import androidx.databinding.library.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class UnlimStorageApp : Application()
+class UnlimStorageApp : Application() {
+	override fun onCreate() {
+		super.onCreate()
+
+		if (BuildConfig.DEBUG) {
+			Timber.plant(Timber.DebugTree())
+		}
+	}
+}
