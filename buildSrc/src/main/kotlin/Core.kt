6de378ce47666a83f0d42c0bfb991883105ab953
Vladislav Daniliuk <48360685+VladDaniliuk@users.noreply.github.com>
2021-09-06 10:13:43 +0300

    Change application icon. Add Splash screen.

--- buildSrc/src/main/kotlin/Core.kt
+++ buildSrc/src/main/kotlin/Core.kt
@@ -3,14 +3,18 @@ import org.gradle.kotlin.dsl.dependencies
 
 object CoreVersion {
 	const val coreKtxVersion = "1.6.0"
+	const val coreSplashScreenVersion = "1.0.0-alpha01"
 }
 
 object CoreLib {
 	const val coreKtx = "androidx.core:core-ktx:${CoreVersion.coreKtxVersion}"
+	const val coreSplashScreen =
+		"androidx.core:core-splashscreen:${CoreVersion.coreSplashScreenVersion}"
 }
 
 fun Project.implementCore() {
 	dependencies {
 		add("implementation", CoreLib.coreKtx)
+		add("implementation", CoreLib.coreSplashScreen)
 	}
 }
