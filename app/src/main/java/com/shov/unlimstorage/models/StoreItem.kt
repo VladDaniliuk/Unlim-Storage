dd4973d0b787004460914945d3d581d1150b1f12
Vladislav Daniliuk <shapeofvoice2001@gmail.com>
2021-10-05 12:00:05 +0300

    StoreItem to Parcelable. Create Enum class itemType. Change Composable StoreItem.

--- app/src/main/java/com/shov/unlimstorage/models/StoreItem.kt
+++ app/src/main/java/com/shov/unlimstorage/models/StoreItem.kt
@@ -1,12 +1,18 @@
 package com.shov.unlimstorage.models
 
+import android.os.Parcel
+import android.os.Parcelable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Description
+import androidx.compose.material.icons.rounded.Folder
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.shov.unlimstorage.models.signInModels.StorageType
 
-enum class ItemType {
-	FILE, FOLDER
+enum class ItemType(val imageVector: ImageVector) {
+	FILE(Icons.Rounded.Description), FOLDER(Icons.Rounded.Folder);
 }
 
 @Entity
@@ -17,4 +23,36 @@ data class StoreItem(
 	@ColumnInfo val size: String? = null,
 	@ColumnInfo val parentFolder: String? = null,
 	@ColumnInfo val disk: StorageType
-)
+) : Parcelable {
+	constructor(parcel: Parcel) : this(
+		parcel.readString().toString(),
+		ItemType.valueOf(parcel.readString().toString()),
+		parcel.readString().toString(),
+		parcel.readString(),
+		parcel.readString(),
+		StorageType.valueOf(parcel.readString().toString())
+	)
+
+	override fun writeToParcel(parcel: Parcel, flags: Int) {
+		parcel.writeString(id)
+		parcel.writeString(type.name)
+		parcel.writeString(name)
+		parcel.writeString(size)
+		parcel.writeString(parentFolder)
+		parcel.writeString(disk.name)
+	}
+
+	override fun describeContents(): Int {
+		return 0
+	}
+
+	companion object CREATOR : Parcelable.Creator<StoreItem> {
+		override fun createFromParcel(parcel: Parcel): StoreItem {
+			return StoreItem(parcel)
+		}
+
+		override fun newArray(size: Int): Array<StoreItem?> {
+			return arrayOfNulls(size)
+		}
+	}
+}
