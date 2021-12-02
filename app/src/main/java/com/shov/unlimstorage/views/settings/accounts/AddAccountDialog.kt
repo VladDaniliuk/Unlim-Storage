5f1c31e900bfd1dd07f5cdc7ed96bf1f5290b5ac
Vladislav Daniliuk <shapeofvoice2001@gmail.com>
2021-12-02 16:06:31 +0300

    - Resolve problems.

--- app/src/main/java/com/shov/unlimstorage/views/settings/accounts/AddAccountDialog.kt
+++ app/src/main/java/com/shov/unlimstorage/views/settings/accounts/AddAccountDialog.kt
@@ -1,5 +1,6 @@
 package com.shov.unlimstorage.views.settings.accounts
 
+import android.content.Context
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
@@ -9,22 +10,38 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.window.Dialog
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
+import com.dropbox.core.android.Auth
+import com.dropbox.core.json.JsonReadException
+import com.dropbox.core.oauth.DbxCredential
 import com.shov.unlimstorage.R
+import com.shov.unlimstorage.models.preferences.Preference
 import com.shov.unlimstorage.models.repositories.signIn.StorageType
 import com.shov.unlimstorage.ui.AccountMenuLink
+import com.shov.unlimstorage.values.DROPBOX_CREDENTIAL
 import com.shov.unlimstorage.values.MEDIUM_SHAPES
 import com.shov.unlimstorage.values.PADDING_SMALL_PLUS
-import com.shov.unlimstorage.viewModels.settings.AccountsViewModel
 import com.shov.unlimstorage.viewModels.SignInViewModel
+import com.shov.unlimstorage.viewModels.settings.AccountsViewModel
 
 @Composable
-fun AddAccountDialog(accountsViewModel: AccountsViewModel, signInViewModel: SignInViewModel) {
+fun AddAccountDialog(
+	accountsViewModel: AccountsViewModel = hiltViewModel(),
+	signInViewModel: SignInViewModel = hiltViewModel(),
+	context: Context = LocalContext.current,
+	lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current,
+) {
 	Dialog(onDismissRequest = { accountsViewModel.showAddAccountBottomSheet() }) {
 		Column(
 			modifier = Modifier.background(
@@ -46,7 +63,7 @@ fun AddAccountDialog(accountsViewModel: AccountsViewModel, signInViewModel: Sign
 					) { result ->
 						signInViewModel.checkAccessWithResult(result, storageType)
 						accountsViewModel.setIsAllSignedIn(true)
-						accountsViewModel.showAddAccountBottomSheet(null)
+						accountsViewModel.showAddAccountBottomSheet()
 					}
 
 					AccountMenuLink(
@@ -67,8 +84,5 @@ fun AddAccountDialog(accountsViewModel: AccountsViewModel, signInViewModel: Sign
 @Preview
 @Composable
 fun AddAccountBottomSheetPreview() {
-	AddAccountDialog(
-		accountsViewModel = hiltViewModel(),
-		signInViewModel = hiltViewModel()
-	)
+	AddAccountDialog()
 }
