4679e4d58d6ed9741f9d62cfd847d9dadc789ff6
Vladislav Daniliuk <shapeofvoice2001@gmail.com>
2021-12-02 15:53:56 +0300

    - Resolve problem.

--- app/src/main/java/com/shov/unlimstorage/views/navigations/SettingsNavigation.kt
+++ app/src/main/java/com/shov/unlimstorage/views/navigations/SettingsNavigation.kt
@@ -5,6 +5,7 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.shov.unlimstorage.values.Screen
+import com.shov.unlimstorage.viewModels.provider.singletonViewModel
 import com.shov.unlimstorage.views.settings.SettingsScreen
 import com.shov.unlimstorage.views.settings.accounts.AccountsScreen
 import com.shov.unlimstorage.views.settings.updates.UpdateScreen
