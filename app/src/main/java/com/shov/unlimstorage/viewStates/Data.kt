a5680c1e803454df8fecafe3897a6934b2f02576
Vladislav Daniliuk <shapeofvoice2001@gmail.com>
2021-12-16 18:29:40 +0300

    - Create ChooseDriveBottomSheet.kt.  - Update Data.kt and Remembers.kt.

--- app/src/main/java/com/shov/unlimstorage/viewStates/Data.kt
+++ app/src/main/java/com/shov/unlimstorage/viewStates/Data.kt
@@ -1,13 +1,22 @@
 package com.shov.unlimstorage.viewStates
 
 import android.content.Context
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.hapticfeedback.HapticFeedback
 import androidx.compose.ui.platform.ClipboardManager
 import androidx.compose.ui.platform.UriHandler
+import com.shov.unlimstorage.models.repositories.signIn.StorageType
 
 data class FileInfoState(
 	val clipboardManager: ClipboardManager,
 	val context: Context,
 	val hapticFeedback: HapticFeedback,
 	val uriHandler: UriHandler
+)
+
+data class NewFolderBottomSheetState(
+	val focusRequester: FocusRequester,
+	var storageType: MutableState<StorageType?>,
+	val text: MutableState<String>
 )
--- app/src/main/java/com/shov/unlimstorage/viewStates/Remembers.kt
+++ app/src/main/java/com/shov/unlimstorage/viewStates/Remembers.kt
@@ -2,9 +2,13 @@ package com.shov.unlimstorage.viewStates
 
 import android.content.Context
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.hapticfeedback.HapticFeedback
 import androidx.compose.ui.platform.*
+import com.shov.unlimstorage.models.repositories.signIn.StorageType
 
 
 @Composable
@@ -15,4 +19,13 @@ fun rememberFileInfoState(
 	uriHandler: UriHandler = LocalUriHandler.current
 ) = remember {
 	FileInfoState(clipboardManager, context, hapticFeedback, uriHandler)
+}
+
+@Composable
+fun rememberNewFolderBottomSheet(
+	focusRequester: FocusRequester = remember { FocusRequester() },
+	storageType: StorageType?,
+	text: MutableState<String> = remember { mutableStateOf("") }
+) = remember(focusRequester, storageType, text) {
+	NewFolderBottomSheetState(focusRequester, mutableStateOf(storageType), text)
 }
