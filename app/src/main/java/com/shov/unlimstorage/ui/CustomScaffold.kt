7cd9fcf587e06e2a989e258fd41827585a2579b4
Vladislav Daniliuk <shapeofvoice2001@gmail.com>
2021-12-17 10:49:31 +0300

    - Create FABScaffold for using FloatingActionButton.

--- app/src/main/java/com/shov/unlimstorage/ui/CustomScaffold.kt
+++ app/src/main/java/com/shov/unlimstorage/ui/CustomScaffold.kt
@@ -1,9 +1,9 @@
 package com.shov.unlimstorage.ui
 
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Snackbar
-import androidx.compose.material.SnackbarHost
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.google.accompanist.insets.navigationBarsPadding
@@ -27,5 +27,23 @@ fun CustomScaffold(
 		},
 		scaffoldState = scaffoldViewModel.scaffoldState,
 		topBar = { MainTopBar() }
+	)
+}
+
+@Composable
+fun FABScaffold(onClick: () -> Unit, content: @Composable (PaddingValues) -> Unit) {
+	Scaffold(
+		floatingActionButton = {
+			FloatingActionButton(
+				modifier = Modifier.navigationBarsPadding(),
+				onClick = onClick
+			) {
+				Icon(
+					imageVector = Icons.Rounded.Add,
+					contentDescription = Icons.Rounded.Add.name
+				)
+			}
+		},
+		content = content
 	)
 }
