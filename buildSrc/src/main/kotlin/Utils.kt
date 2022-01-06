d7649fe527afd61fb5f6388227fb9743bab56e30
Vladislav Daniliuk <shapeofvoice2001@gmail.com>
2022-01-06 11:12:41 +0300

    - Update libraries implementation.

--- buildSrc/src/main/kotlin/Utils.kt
+++ buildSrc/src/main/kotlin/Utils.kt
@@ -1,13 +1,8 @@
 import org.gradle.api.artifacts.dsl.DependencyHandler
 import org.gradle.kotlin.dsl.DependencyHandlerScope
-import org.gradle.kotlin.dsl.add
-import org.gradle.kotlin.dsl.exclude
 import org.gradle.kotlin.dsl.support.delegates.DependencyHandlerDelegate
 
-fun DependencyHandler.implement(lib: String, group: String? = null) = add("implementation", lib) {
-	group?.let(::exclude)
-}
-
+fun DependencyHandler.implement(lib: String) = add("implementation", lib)
 fun DependencyHandlerDelegate.kapt(lib: String) = add("kapt", lib)
 fun DependencyHandlerScope.androidTestImplement(lib: String) = add("androidTestImplementation", lib)
 fun DependencyHandlerScope.testImplement(lib: String) = add("testImplementation", lib)
