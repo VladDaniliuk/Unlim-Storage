0804ee1c8e49cb4a74dd9b0d2f7e4446002fc661
Vladislav Daniliuk <shapeofvoice2001@gmail.com>
2022-01-11 14:47:48 +0300

    - Up version.  - Update libraries versions.

--- buildSrc/src/main/kotlin/Utils.kt
+++ buildSrc/src/main/kotlin/Utils.kt
@@ -1,12 +1,8 @@
-import org.gradle.api.artifacts.ExternalModuleDependency
 import org.gradle.api.artifacts.dsl.DependencyHandler
 import org.gradle.kotlin.dsl.DependencyHandlerScope
-import org.gradle.kotlin.dsl.add
 import org.gradle.kotlin.dsl.support.delegates.DependencyHandlerDelegate
 
-fun DependencyHandlerScope.implement(lib: String) = add("implementation", lib)
-
-fun DependencyHandler.implement(lib: String, dependency: ExternalModuleDependency.() -> Unit) =
-	add("implementation", lib, dependency)
-
+fun DependencyHandler.implement(lib: String) = add("implementation", lib)
 fun DependencyHandlerDelegate.kapt(lib: String) = add("kapt", lib)
+fun DependencyHandlerScope.androidTestImplement(lib: String) = add("androidTestImplementation", lib)
+fun DependencyHandlerScope.testImplement(lib: String) = add("testImplementation", lib)
