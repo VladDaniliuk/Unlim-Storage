4caa732ca3373fb31d005b385e895bfdd27bb494
Vladislav Daniliuk <shapeofvoice2001@gmail.com>
2022-01-18 12:34:27 +0300

    - Resolve problems with imports.

--- app/src/main/java/com/shov/unlimstorage/ui/UserInfo.kt
+++ app/src/main/java/com/shov/unlimstorage/ui/UserInfo.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.shov.unlimstorage.ui.icons.LoadIcon
 import com.shov.unlimstorage.ui.texts.CustomText
 
 @OptIn(ExperimentalFoundationApi::class)
