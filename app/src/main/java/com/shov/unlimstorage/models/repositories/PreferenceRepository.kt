5efd0332b5f0c44f06c097e1501c278bd60908d6
Vladislav Daniliuk <shapeofvoice2001@gmail.com>
2022-01-21 13:32:03 +0300

    - Create PreferenceRepository.kt.

---
+++ app/src/main/java/com/shov/unlimstorage/models/repositories/PreferenceRepository.kt
@@ -0,0 +1,16 @@
+package com.shov.unlimstorage.models.repositories
+
+import android.content.Context
+import com.shov.unlimstorage.models.preferences.Preference
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+
+interface PreferenceRepository {
+	fun <T> getPref(name: String, default: T): Preference<T>
+}
+
+class PreferenceRepositoryImpl @Inject constructor(
+	@ApplicationContext val context: Context
+) : PreferenceRepository {
+	override fun <T> getPref(name: String, default: T) = Preference(context, name, default)
+}
