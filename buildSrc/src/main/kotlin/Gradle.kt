4dcb101a46d0068591d308cb6b00a9913fd26afd
Vladislav Daniliuk <shapeofvoice2001@gmail.com>
2022-01-25 10:42:33 +0300

    - Up version of Gradle.  - Resolve problem with spacing for CustomDialog.kt.

--- buildSrc/src/main/kotlin/Gradle.kt
+++ buildSrc/src/main/kotlin/Gradle.kt
@@ -2,7 +2,7 @@ import org.gradle.kotlin.dsl.ScriptHandlerScope
 
 object Gradle {
 	private const val lib = "com.android.tools.build:gradle:"
-	private const val version = "7.0.3"
+	private const val version = "7.0.4"
 
 	object Lib {
 		const val gradle = "$lib$version"
