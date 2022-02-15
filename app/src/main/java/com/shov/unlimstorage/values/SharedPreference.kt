649ed0c1ef16c1bb75fbeb5aa462d79f17860bfd
Vladislav Daniliuk <shapeofvoice2001@gmail.com>
2022-02-15 15:28:12 +0300

    - Update preferences values.

--- app/src/main/java/com/shov/unlimstorage/values/SharedPreference.kt
+++ app/src/main/java/com/shov/unlimstorage/values/SharedPreference.kt
@@ -2,8 +2,10 @@ package com.shov.unlimstorage.values
 
 //Shared preferences
 const val PREFERENCES = "PREFERENCES"
+const val ENCRYPTED_PREFERENCE = "ENCRYPTED_PREFERENCE"
 
 //Preference arguments
+const val DROPBOX_CREDENTIAL = "DROPBOX_CREDENTIAL"
 const val IS_AUTH = "IS_AUTH"
 const val DROPBOX_CREDENTIAL = "DROPBOX_CREDENTIAL"
 const val IS_UPDATE_SHOW = "IS_UPDATE_SHOW"
