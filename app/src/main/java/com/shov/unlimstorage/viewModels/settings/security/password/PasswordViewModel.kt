4eeef2f177f371b2c879bcc96800239c17577a5b
Vladislav Daniliuk <shapeofvoice2001@gmail.com>
2022-02-16 19:54:09 +0300

    - Create Password Screen.

---
+++ app/src/main/java/com/shov/unlimstorage/viewModels/settings/security/password/PasswordViewModel.kt
@@ -0,0 +1,38 @@
+package com.shov.unlimstorage.viewModels.settings.security.password
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class PasswordViewModel @Inject constructor() : ViewModel() {
+	var pass by mutableStateOf("")
+		private set
+	var isPassVisible by mutableStateOf(false)
+		private set
+
+	fun changeVisibility() {
+		isPassVisible = isPassVisible.not()
+	}
+
+	fun changePass(char: Int) {
+		if (pass.length < 8) {
+			pass += char
+		}
+	}
+
+	fun submitPass(onError: () -> Unit, onRightClick: (String) -> Unit) {
+		if (pass.length < 4) onError() else {
+			onRightClick(pass)
+
+			removeLastInt(true)
+		}
+	}
+
+	fun removeLastInt(all: Boolean = false) {
+		pass = if (all) "" else pass.substring(0 until pass.lastIndex)
+	}
+}
---
+++ app/src/main/java/com/shov/unlimstorage/views/settings/security/password/PasswordScreen.kt
@@ -0,0 +1,66 @@
+package com.shov.unlimstorage.views.settings.security.password
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.shov.unlimstorage.ui.themes.customTheme.CustomTheme
+import com.shov.unlimstorage.viewModels.settings.security.password.PasswordViewModel
+
+@Composable
+fun PasswordScreen(
+	modifier: Modifier = Modifier,
+	passwordViewModel: PasswordViewModel = viewModel(),
+	orientation: Int = LocalConfiguration.current.orientation,
+	onError: () -> Unit,
+	onRightClick: (String) -> Unit
+) = when (orientation) {
+	Configuration.ORIENTATION_LANDSCAPE -> Row(modifier = modifier) {
+		PasswordView(
+			isPassVisible = passwordViewModel.isPassVisible,
+			password = passwordViewModel.pass,
+			onChangeVisibility = passwordViewModel::changeVisibility,
+			onLeftClick = passwordViewModel::removeLastInt,
+			isOnLeftClickEnabled = passwordViewModel.pass.isNotEmpty(),
+			onLeftLongClick = { passwordViewModel.removeLastInt(true) },
+			onRightClick = {
+				passwordViewModel.submitPass(
+					onError = onError,
+					onRightClick = onRightClick
+				)
+			},
+			onClick = passwordViewModel::changePass
+		)
+	}
+	else -> Column(modifier = modifier) {
+		PasswordView(
+			isPassVisible = passwordViewModel.isPassVisible,
+			password = passwordViewModel.pass,
+			onChangeVisibility = passwordViewModel::changeVisibility,
+			onLeftClick = passwordViewModel::removeLastInt,
+			isOnLeftClickEnabled = passwordViewModel.pass.isNotEmpty(),
+			onLeftLongClick = { passwordViewModel.removeLastInt(true) },
+			onRightClick = {
+				passwordViewModel.submitPass(
+					onError = onError,
+					onRightClick = onRightClick
+				)
+			},
+			onClick = passwordViewModel::changePass
+		)
+	}
+}
+
+@Preview
+@Composable
+fun PasswordScreenPreview() {
+	CustomTheme {
+		PasswordScreen(onError = {}, passwordViewModel = hiltViewModel()) {}
+	}
+}
+
