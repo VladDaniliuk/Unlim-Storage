f46077da3c6505219e147d09da47aa6b1359cf4b
Vladislav Daniliuk <shapeofvoice2001@gmail.com>
2022-02-16 19:30:04 +0300

    - Move getBackStack to NavHostControllerUtil.kt.

--- app/src/main/java/com/shov/unlimstorage/utils/NavHostControllerUtil.kt
+++ app/src/main/java/com/shov/unlimstorage/utils/NavHostControllerUtil.kt
@@ -1,7 +1,21 @@
 package com.shov.unlimstorage.utils
 
 import androidx.navigation.NavHostController
+import com.shov.unlimstorage.models.items.BackStack
+import com.shov.unlimstorage.values.argFolderId
+import com.shov.unlimstorage.values.argFolderName
+import com.shov.unlimstorage.values.argStorageType
 
 fun NavHostController.popBackStack(size: Int) {
 	for (i in 0 until size) popBackStack()
 }
+
+fun NavHostController.getBackStack() = currentBackStackEntry?.arguments?.run {
+	BackStack(
+		getString(argFolderId) ?: throw IllegalArgumentException("No folder id"),
+		getString(argStorageType) ?: throw IllegalArgumentException("No storage type"),
+		getString(argFolderName) ?: throw IllegalArgumentException("No folder name")
+	)
+}
+
+fun NavHostController.navigateTo(route: String) = navigate(route)
