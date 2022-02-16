cfd28dfc29b3993cf94d5f0aad3f3134d9fe9ef1
Vladislav Daniliuk <shapeofvoice2001@gmail.com>
2022-02-16 19:28:08 +0300

    - Remove unnecessary remember.

--- app/src/main/java/com/shov/unlimstorage/viewStates/Remembers.kt
+++ app/src/main/java/com/shov/unlimstorage/viewStates/Remembers.kt
@@ -4,22 +4,11 @@ import android.content.Context
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.hapticfeedback.HapticFeedback
-import androidx.compose.ui.platform.*
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.shov.unlimstorage.models.repositories.signIn.StorageType
 import kotlinx.coroutines.CoroutineScope
-
-@Composable
-fun rememberFileInfoState(
-	clipboardManager: ClipboardManager = LocalClipboardManager.current,
-	context: Context = LocalContext.current,
-	hapticFeedback: HapticFeedback = LocalHapticFeedback.current,
-	uriHandler: UriHandler = LocalUriHandler.current
-) = remember {
-	FileInfoState(clipboardManager, context, hapticFeedback, uriHandler)
-}
 
 @Composable
 fun rememberFilesScreenState(
