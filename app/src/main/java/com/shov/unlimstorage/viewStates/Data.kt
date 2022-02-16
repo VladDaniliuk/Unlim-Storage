412eb7ea8da292dda8a50a999f2669565cdd6ead
Vladislav Daniliuk <shapeofvoice2001@gmail.com>
2022-02-16 19:46:48 +0300

    - Remove unused data class.

--- app/src/main/java/com/shov/unlimstorage/viewStates/Data.kt
+++ app/src/main/java/com/shov/unlimstorage/viewStates/Data.kt
@@ -1,20 +1,10 @@
 package com.shov.unlimstorage.viewStates
 
 import android.content.Context
-import androidx.compose.ui.hapticfeedback.HapticFeedback
-import androidx.compose.ui.platform.ClipboardManager
-import androidx.compose.ui.platform.UriHandler
 import androidx.navigation.NavHostController
 import com.shov.unlimstorage.models.repositories.signIn.StorageType
 import kotlinx.coroutines.CoroutineScope
 import java.io.InputStream
-
-data class FileInfoState(
-	val clipboardManager: ClipboardManager,
-	val context: Context,
-	val hapticFeedback: HapticFeedback,
-	val uriHandler: UriHandler
-)
 
 data class FilesScreenState(
 	val navHostController: NavHostController,
