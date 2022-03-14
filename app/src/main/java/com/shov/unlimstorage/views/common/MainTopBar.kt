6e2c35b8c163566a2fcad8cda7db289a33585ab4
Vladislav Daniliuk <48360685+VladDaniliuk@users.noreply.github.com>
2022-03-14 17:05:19 +0300

    - Resolve problems with CustomTopAppBar.kt.

--- app/src/main/java/com/shov/unlimstorage/views/common/MainTopBar.kt
+++ app/src/main/java/com/shov/unlimstorage/views/common/MainTopBar.kt
@@ -2,7 +2,7 @@ package com.shov.unlimstorage.views.common
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import com.shov.unlimstorage.ui.CustomTopAppBar
+import com.shov.coreui.ui.CustomTopAppBar
 import com.shov.unlimstorage.viewModels.common.TopAppBarViewModel
 import com.shov.unlimstorage.viewModels.provider.singletonViewModel
 
@@ -11,11 +11,11 @@ fun MainTopBar(topAppBarViewModel: TopAppBarViewModel = singletonViewModel()) {
 	CustomTopAppBar(
 		prevRouteImageVector = topAppBarViewModel.topAppBar.prevRoute?.first,
 		onPrevRouteClick = topAppBarViewModel.topAppBar.prevRoute?.second ?: {},
-		prevRouteVisible = topAppBarViewModel.topAppBar.prevRoute != null,
+		prevRouteEnabled = topAppBarViewModel.topAppBar.prevRoute != null,
 		title = topAppBarViewModel.topAppBar.title,
 		nextRouteImageVector = topAppBarViewModel.topAppBar.nextRoute?.first,
 		onNextRouteClick = topAppBarViewModel.topAppBar.nextRoute?.second ?: {},
-		nextRouteVisible = topAppBarViewModel.topAppBar.nextRoute != null
+		nextRouteEnabled = topAppBarViewModel.topAppBar.nextRoute != null
 	)
 
 	LaunchedEffect(key1 = topAppBarViewModel.topAppBar.prevRoute) {
