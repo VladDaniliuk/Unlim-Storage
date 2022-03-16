0b10150bb823afd23c66a56975135ad192c3eb3c
Vladislav Daniliuk <48360685+VladDaniliuk@users.noreply.github.com>
2022-03-16 16:36:40 +0300

    - Use IS_AUTH value from preferences module.

--- app/src/main/java/com/shov/unlimstorage/values/SharedPreference.kt
+++ app/src/main/java/com/shov/unlimstorage/values/SharedPreference.kt
@@ -4,6 +4,5 @@ package com.shov.unlimstorage.values
 const val PIN_CODE = "PIN_CODE"
 
 //Preference arguments
-const val IS_AUTH = "IS_AUTH"
 const val IS_BIOMETRIC_ENABLED = "IS_BIOMETRIC_ENABLED"
 const val IS_UPDATE_SHOW = "IS_UPDATE_SHOW"
--- app/src/main/java/com/shov/unlimstorage/viewModels/navigations/MainNavigationViewModel.kt
+++ app/src/main/java/com/shov/unlimstorage/viewModels/navigations/MainNavigationViewModel.kt
@@ -2,7 +2,7 @@ package com.shov.unlimstorage.viewModels.navigations
 
 import androidx.lifecycle.ViewModel
 import com.shov.preferences.datasources.PreferencesDataSource
-import com.shov.unlimstorage.values.IS_AUTH
+import com.shov.preferences.values.IS_AUTH
 import com.shov.unlimstorage.values.PIN_CODE
 import com.shov.unlimstorage.values.Screen
 import dagger.hilt.android.lifecycle.HiltViewModel
