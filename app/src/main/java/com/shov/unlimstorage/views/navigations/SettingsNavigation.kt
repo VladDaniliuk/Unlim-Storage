5d72386ee6890a6b5aa030cb0a1e1f73626dd1d0
Vladislav Daniliuk <48360685+VladDaniliuk@users.noreply.github.com>
2022-03-17 18:12:39 +0300

    - Update imports in SettingsNavigation.kt.

--- app/src/main/java/com/shov/unlimstorage/views/navigations/SettingsNavigation.kt
+++ app/src/main/java/com/shov/unlimstorage/views/navigations/SettingsNavigation.kt
@@ -3,9 +3,9 @@ package com.shov.unlimstorage.views.navigations
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
+import com.shov.signinfeature.views.AccountsScreen
 import com.shov.unlimstorage.values.Screen
 import com.shov.unlimstorage.views.settings.SettingsScreen
-import com.shov.signinfeature.views.AccountsScreen
 import com.shov.unlimstorage.views.settings.updates.UpdateScreen
 
 fun NavGraphBuilder.settingsComposable(settingsNavController: NavHostController) {
