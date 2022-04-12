fb75dc763a6283468103018b9bd295e5f818af40
Vladislav Daniliuk <48360685+VladDaniliuk@users.noreply.github.com>
2022-04-12 13:08:09 +0300

    - Remove unused material 2 theme colors.

--- coreUI/src/main/kotlin/com/shov/coreui/ui/themes/customTheme/DarkTheme.kt
+++ coreUI/src/main/kotlin/com/shov/coreui/ui/themes/customTheme/DarkTheme.kt
@@ -1,6 +1,5 @@
 package com.shov.coreui.ui.themes.customTheme
 
-import androidx.compose.material.Colors
 import androidx.compose.material3.darkColorScheme
 import com.shov.coreui.ui.themes.*
 
@@ -31,20 +30,4 @@ val DarkThemeColors = darkColorScheme(
 	inverseOnSurface = md_theme_dark_inverseOnSurface,
 	inverseSurface = md_theme_dark_inverseSurface,
 	inversePrimary = md_theme_dark_inversePrimary,
-)
-
-val DarkTheme = Colors(
-	primary = md_theme_dark_primary,
-	primaryVariant = md_theme_dark_primaryContainer,
-	onPrimary = md_theme_dark_onPrimary,
-	secondary = md_theme_dark_secondary,
-	secondaryVariant = md_theme_dark_secondaryContainer,
-	onSecondary = md_theme_dark_onSecondary,
-	background = md_theme_dark_background,
-	error = md_theme_dark_error,
-	surface = md_theme_dark_surface,
-	onBackground = md_theme_dark_onBackground,
-	onSurface = md_theme_dark_onSurface,
-	onError = md_theme_dark_onError,
-	isLight = false
 )
--- coreUI/src/main/kotlin/com/shov/coreui/ui/themes/customTheme/LightTheme.kt
+++ coreUI/src/main/kotlin/com/shov/coreui/ui/themes/customTheme/LightTheme.kt
@@ -1,6 +1,5 @@
 package com.shov.coreui.ui.themes.customTheme
 
-import androidx.compose.material.Colors
 import androidx.compose.material3.lightColorScheme
 import com.shov.coreui.ui.themes.*
 
@@ -31,20 +30,4 @@ val LightThemeColors = lightColorScheme(
 	inverseOnSurface = md_theme_light_inverseOnSurface,
 	inverseSurface = md_theme_light_inverseSurface,
 	inversePrimary = md_theme_light_inversePrimary,
-)
-
-val LightTheme = Colors(
-	primary = md_theme_light_primary,
-	primaryVariant = md_theme_light_primaryContainer,
-	onPrimary = md_theme_light_onPrimary,
-	secondary = md_theme_light_secondary,
-	secondaryVariant = md_theme_light_secondaryContainer,
-	onSecondary = md_theme_light_onSecondary,
-	background = md_theme_light_background,
-	error = md_theme_light_error,
-	surface = md_theme_light_surface,
-	onBackground = md_theme_light_onBackground,
-	onSurface = md_theme_light_onSurface,
-	onError = md_theme_light_onError,
-	isLight = true
 )
