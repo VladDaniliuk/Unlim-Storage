ed07ce7d638c471692a1ee2d91b72d82b6d2b7d1
Vladislav Daniliuk <shapeofvoice2001@gmail.com>
2022-04-27 11:35:08 +0300

    - Update method for creating singleton instance of viewModel.

--- coreUtils/src/main/kotlin/com/shov/coreutils/viewModels/ViewModelsProvider.kt
+++ coreUtils/src/main/kotlin/com/shov/coreutils/viewModels/ViewModelsProvider.kt
@@ -3,12 +3,17 @@ package com.shov.coreutils.viewModels
 import androidx.activity.ComponentActivity
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 
+/**
+ * Method for creating a singleton [ViewModel] instance for the given [ComponentActivity].
+ * @param VM the type of the ViewModel to create.
+ * @property key the key to create different instances of [ViewModel] or use singleton instance.
+ * */
 @Composable
-inline fun <reified VM : ViewModel> singletonViewModel(): VM {
+inline fun <reified VM : ViewModel> singletonViewModel(key: String? = VM::class.simpleName): VM {
 	val context = LocalContext.current as ComponentActivity
 
-	return hiltViewModel(context)
+	return viewModel(context, key)
 }
