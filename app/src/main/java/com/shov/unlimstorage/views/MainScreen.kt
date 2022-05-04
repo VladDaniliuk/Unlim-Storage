68870e0ffa7dd3150726b13ac4c0751e5ad1412f
Vladislav Daniliuk <shapeofvoice2001@gmail.com>
2022-05-04 16:46:49 +0300

    - Resolve problems with NewVersionDialog.

--- app/src/main/java/com/shov/unlimstorage/views/MainScreen.kt
+++ app/src/main/java/com/shov/unlimstorage/views/MainScreen.kt
@@ -5,15 +5,11 @@ import androidx.navigation.compose.rememberNavController
 import com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi
 import com.shov.autoupdatefeature.views.NewVersionDialog
 import com.shov.coreui.views.CustomScaffold
-import com.shov.coreutils.viewModels.singletonViewModel
-import com.shov.filesfeature.viewModels.DownloadViewModel
 import com.shov.unlimstorage.views.navigations.MainNavigation
 
 @Composable
-fun MainScreen(
-	downloadViewModel: DownloadViewModel = singletonViewModel()
-) {
-	NewVersionDialog(setProgress = downloadViewModel::onDownload)
+fun MainScreen() {
+	NewVersionDialog()
 
 	@OptIn(ExperimentalMaterialNavigationApi::class)
 	CustomScaffold { bottomSheetNavigator ->
